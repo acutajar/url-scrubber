68bbdeb99b342f145604bbca10c5720d95b048a3
acutajar <aleccutajar@gmail.com>
2022-02-08 16:45:06 -0500

    added xlsx file creation and download

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script>
   import * as Papa from "papaparse";
+  import * as XLSX from "xlsx";
   import { badWords, badTLDs } from "./removed-words.js";
+  import { saveAs } from "file-saver";
+  let wbOut = "";
   async function scrub() {
     const uploadedFile = document.getElementById("file-input").files[0];
     Papa.parse(uploadedFile, {
@@ -27,10 +30,10 @@
   const scrubLinks = (arr) => {
     let cleanLinks = [];
     arr.forEach((element) => {
-      if (!checkDomain(element[1]) || !checkTLD(element[1]))
+      if (checkDomain(element[1]) && checkTLD(element[1]))
         cleanLinks.push(element);
     });
-    console.log("clean", cleanLinks);
+    createWorkbook(cleanLinks);
   };
 
   function checkDomain(url) {
@@ -48,13 +51,66 @@
     let tld = "." + url.hostname.split(".").pop();
     return !(tld in badTLDsObj);
   }
+
+  function createWorkbook(array) {
+    const wb = XLSX.utils.book_new();
+    const ws = XLSX.utils.aoa_to_sheet([["Linking to", "URL"], ...array]);
+    XLSX.utils.book_append_sheet(wb, ws, "Scrubbed URLs");
+    wbOut = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+    wbDownload(wbOut);
+  }
+
+  function s2ab(s) {
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf); //create uint8array as viewer
+    for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff; //convert to octet
+    return buf;
+  }
+
+  function wbDownload(data) {
+    let wbTitle = document.getElementById("output-title").value;
+    wbTitle = wbTitle + ".xlsx";
+    saveAs(
+      new Blob([s2ab(data)], { type: "application/octet-stream" }),
+      wbTitle
+    );
+  }
+
+  function handleDragEnter(e) {
+    document.getElementById(e.target.id).style.border = "2px solid red";
+  }
+  function handleDragAndDrop(e) {
+    document.getElementById(e.target.id).style.border = "2px solid red";
+  }
+  function handleDragLeave(e) {
+    document.getElementById(e.target.id).style.border = "2px dashed red";
+  }
 </script>
 
 <main>
   <h1>Hello!</h1>
 
-  <input type="file" id="file-input" />
+  <input
+    type="file"
+    id="file-input"
+    on:dragenter={handleDragEnter}
+    on:dragleave={handleDragLeave}
+    on:drop={handleDragAndDrop}
+  />
+  <form action="">
+    <label for="output-title">Enter title for output file:</label>
+    <input type="text" id="output-title" value="Scrubbed_Urls" />
+  </form>
   <button on:click={scrub}>Scrub</button>
+
+  <!-- <button
+    on:click={function () {
+      saveAs(
+        new Blob([s2ab(data)], { type: "application/octet-stream" }),
+        "test.xlsx"
+      );
+    }}
+  /> -->
 </main>
 
 <style>
@@ -72,7 +128,16 @@
     font-weight: 100;
   }
 
-  @media (min-width: 640px) {
+  #file-input {
+    border: 2px dashed red;
+    width: 80vw;
+    height: 25vh;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 10px;
+  }
+
+  @media (min-width: 600px) {
     main {
       max-width: none;
     }
