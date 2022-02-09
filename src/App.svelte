21f4e1143a79651b0299d6c45e83e4116e15bf91
acutajar <aleccutajar@gmail.com>
2022-02-09 09:37:31 -0500

    fixed issue with blank csv lines

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import { badWords, badTLDs } from "./removed-words.js";
   import { saveAs } from "file-saver";
   let wbOut = "";
+  let removedURLs = [];
   async function scrub() {
     const uploadedFile = document.getElementById("file-input").files[0];
     Papa.parse(uploadedFile, {
@@ -15,12 +16,20 @@
   }
 
   const parseResults = (results) => {
-    let linkingFrom = results.map((item) => item["Linking From"].split("\r\n"));
+    let linkingFrom = results.map((item) => {
+      if (item["Linking From"] != undefined)
+        return item["Linking From"].split("\r\n");
+    });
     linkingFrom = [].concat.apply([], linkingFrom);
-
-    let linkingTo = results.map((item) => item["Linking To"].split("\r\n"));
+    let linkingTo = results.map((item) => {
+      if (item["Linking To"] != undefined)
+        return item["Linking To"].split("\r\n");
+    });
     linkingTo = [].concat.apply([], linkingTo);
-    const linksArr = linkingFrom.map((val, i) => [linkingTo[i], val]);
+    const linksArr = linkingFrom
+      .map((val, i) => [linkingTo[i], val])
+      .filter((item) => item[0] != undefined);
+    console.log(linksArr);
     scrubLinks(linksArr);
   };
 
@@ -30,7 +39,7 @@
   const scrubLinks = (arr) => {
     let cleanLinks = [];
     arr.forEach((element) => {
-      if (checkDomain(element[1]) && checkTLD(element[1]))
+      if (!checkDomain(element[1]) || !checkTLD(element[1]))
         cleanLinks.push(element);
     });
     createWorkbook(cleanLinks);
@@ -39,7 +48,7 @@
   function checkDomain(url) {
     for (let i = 0; i < badWords.length; i++) {
       if (url.includes(badWords[i])) {
-        // console.log(url, badWords[i]);
+        removedURLs.push([url, badWords[i]]);
         return false;
       }
     }
@@ -54,8 +63,16 @@
 
   function createWorkbook(array) {
     const wb = XLSX.utils.book_new();
-    const ws = XLSX.utils.aoa_to_sheet([["Linking to", "URL"], ...array]);
-    XLSX.utils.book_append_sheet(wb, ws, "Scrubbed URLs");
+    const cleanedWs = XLSX.utils.aoa_to_sheet([
+      ["Linking to", "URL"],
+      ...array,
+    ]);
+    const removedWs = XLSX.utils.aoa_to_sheet([
+      ["Removed URL", "Bad Word Found"],
+      ...removedURLs,
+    ]);
+    XLSX.utils.book_append_sheet(wb, cleanedWs, "Scrubbed URLs");
+    XLSX.utils.book_append_sheet(wb, removedWs, "Removed URLs");
     wbOut = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
     wbDownload(wbOut);
   }
@@ -102,15 +119,6 @@
     <input type="text" id="output-title" value="Scrubbed_Urls" />
   </form>
   <button on:click={scrub}>Scrub</button>
-
-  <!-- <button
-    on:click={function () {
-      saveAs(
-        new Blob([s2ab(data)], { type: "application/octet-stream" }),
-        "test.xlsx"
-      );
-    }}
-  /> -->
 </main>
 
 <style>
